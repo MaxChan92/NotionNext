f204bcf361c2a6248d1b12ada6d106fbdfab45d3
MaxChan92 <70808019+MaxChan92@users.noreply.github.com>
2024-04-25 13:15:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Max', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个设计师，喜欢折腾，愿意分享', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.alphamax.cloud', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://himax.me', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客，折腾，分享，', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
